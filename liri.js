9ead1b049bad456ceb2b818f88d70faa16e8ac68
Ana <aparakaki17@gmail.com>
2018-09-16 12:27:04 -0700

    add movie code

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var request = require("request");
 var moment = require('moment');
+var fs = require("fs");
 var Spotify = require("node-spotify-api");
 
 var keys = require("./keys.js");
@@ -10,6 +11,16 @@ var spotify = new Spotify(keys.spotify);
 
 var option = process.argv[2];
 var userInput = process.argv.splice(3).join(" ");
+
+if (option === "do-what-it-says") {
+    console.log("if loop")
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        option = data.split(",")[0];
+        userInput = data.split(",")[1];
+        console.log("1" +option);
+    })
+    console.log("2" +option);
+}
 
 switch(option) {
     case "concert-this":
@@ -38,7 +49,32 @@ switch(option) {
     break;
      
     case "movie-this":
-    break;
+        if (userInput === "") {
+            userInput = "Mr. Nobody"
+            console.log("* If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("* It's on Netflix!");
+        }
+        else {
+            request("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+                var info = JSON.parse(body);
+                console.log("* Title: " + info.Title);
+                console.log("* Year: " + info.Year);
+                console.log("* IMDB Rating: " + info.imdbRatings);
+                for (var i = 0; i < info.Ratings.length; i++) {
+                    if (info.Ratings[i].Source === "Rotten Tomatoes") {
+                        console.log("* Rotten Tomatoes Rating: " + info.Ratings[i].Value);
+                    }
+                    // else {
+                    //     console.log("No Rotten Tomatoes Rating available");
+                    // }
+                }
+                console.log("* Country: " + info.Country);
+                console.log("* Language: " + info.Language);
+                console.log("* Plot: " + info.Plot);
+                console.log("* Actors: " + info.Actors);
+            })
+        }
+        break;
 
     default:            // do-what-it-says
     break;
